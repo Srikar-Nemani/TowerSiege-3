10d158f245012e1eac090d6cbe93f6611a6e3850
Srikar-Nemani <74164163+Srikar-Nemani@users.noreply.github.com>
2021-01-17 09:48:33 -0800

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,30 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+    var options = {
+    bodyA: bodyA,
+    pointB:pointB,
+    stiffness: 0.05,
+    length: 10
+    }
+    this.pointB=pointB;
+    this.sling = Constraint.create(options);
+    World.add(world, this.sling);
+    }
+    fly(){
+    this.sling.bodyA=null;    
+    }
+
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
+    display(){
+    if(this.sling.bodyA){
+    var posA = this.sling.bodyA.position;
+    var posB = this.pointB;
+    strokeWeight(4);
+    stroke("white");
+    line(posA.x, posA.y, posB.x, posB.y);
+    }
+    }   
+    }
